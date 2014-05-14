16616921adaba7de8dc4d8e23554a030cde7c7b5
xzavier <xzavier.dev@gmail.com>
2014-05-14 11:25:27 +0100

    Test git rm command

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -3,5 +3,7 @@ using System.Collections;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour {
+    public float moveSpeed = 0;
+    public float jumpSpeed = 0;
 
 }
