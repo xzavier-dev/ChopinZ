2f0b9b1249c755b482e8767d40cc8151471c9656
xzavier <xzavier.dev@gmail.com>
2014-05-16 18:32:13 +0800

    Add Player Move Test Script

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,6 +4,36 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour {
     public float moveSpeed = 0;
-    public float jumpSpeed = 0;
+    public float jumpHeight = 0;
+    public float gravity = 9.8f;
+
+    private CharacterController _controller = null;
+
+    void Start() {
+        _controller = GetComponent<CharacterController>();
+        if ( _controller == null ) {
+            Debug.LogError( "ERROR: Cannot get character controller" );
+            this.enabled = false;
+        }
+    }
+
+    void Update() {
+        float h = Input.GetAxis( "Horizontal" );
+        Vector3 dir = Vector3.zero;
+        if ( _controller.isGrounded ) {
+            dir = new Vector3( h, 0, 0 );
+            dir = transform.TransformDirection( dir );
+            dir *= moveSpeed;
+
+            if ( Input.GetKeyDown( KeyCode.Space ) ) {
+                dir.y = jumpHeight;
+            }
+
+        }
+
+        dir.y -= gravity * Time.deltaTime;
+
+        _controller.Move( dir * Time.deltaTime);
+    }
 
 }
---
+++ Assets/Scripts/Player/RigidbodyMove.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MoveState_t {
+    Grounded,
+    JumpUp,
+    JumpDown,
+}
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodyMove : MonoBehaviour {
+    public float moveSpeed = 0;
+    public float jumpSpeed = 0;
+    public float gravity = 15;
+
+    private bool _isGrounded = false;
+    private Vector3 velocity = Vector3.zero;
+    
+    public MoveState_t moveState;
+    private float _startJumpDownTime = 0;
+    private float _startJumpUpTime = 0;
+    private float _lastVelocityX = 0;
+    private float _maxMoveSpeedOfAir;
+
+
+	void Start () {
+        ChangeMoveState( MoveState_t.JumpDown );
+        _maxMoveSpeedOfAir = moveSpeed / 1.5f;
+	}
+	
+
+	void Update () {
+
+        float h = Input.GetAxis( "Horizontal" );
+
+        switch(moveState) {
+            case MoveState_t.Grounded:
+                rigidbody.velocity = new Vector3( h * moveSpeed, 0, 0 );
+                break;
+            case MoveState_t.JumpUp:
+                rigidbody.velocity = new Vector3( Mathf.Clamp(_lastVelocityX,_lastVelocityX,_maxMoveSpeedOfAir), jumpSpeed + gravity * Mathf.Sqrt( Time.time - _startJumpUpTime ), 0 );
+                break;
+        }
+
+        if ( Input.GetKeyDown( KeyCode.Space ) ) {
+            ChangeMoveState( MoveState_t.JumpUp );
+        }
+	
+	}
+
+
+    void ChangeMoveState(MoveState_t state) {
+        switch (state ){
+            case MoveState_t.Grounded:
+
+                break;
+            case MoveState_t.JumpDown:
+                _startJumpDownTime = Time.time;
+                break;
+            case MoveState_t.JumpUp:
+                if( moveState != MoveState_t.Grounded ){
+                    return;
+                }
+                _startJumpUpTime = Time.time;
+                _lastVelocityX = rigidbody.velocity.x;
+               
+                break;
+        }
+        moveState = state;
+    }
+
+
+    void OnCollisionEnter( Collision obj ) {
+        if ( obj.gameObject.CompareTag( "Ground" ) ) {
+            ChangeMoveState( MoveState_t.Grounded );
+        }
+    }
+
+}
